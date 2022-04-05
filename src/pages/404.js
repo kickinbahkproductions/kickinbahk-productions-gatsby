80efa3f81f6a601b814688937e0fcf38c6f7c372
kickinbahk <kickinbahk@gmail.com>
2022-04-05 10:18:55 -0600

    add sections

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,53 @@
-import * as React from "react"
+import React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
+import Section from "sections/section"
 
-const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulLayout(slug: { eq: "404" }) {
+        id
+        title
+        description
+        contentful_id
+        menu {
+          name
+          type
+          menuItems {
+            id
+            title
+            url
+          }
+        }
+        contentModule {
+          ... on Node {
+            id
+          }
+        }
+      }
+    }
+  `)
+
+  const title = data.contentfulLayout.title
+  const description = data.contentfulLayout.description
+
+  const menus = data.contentfulLayout.menu
+
+  const contentModule = data.contentfulLayout.contentModule
+
+  return (
+    <Layout menus={menus}>
+      <SEO title={title} description={description} />
+      {contentModule &&
+        contentModule.length > 0 &&
+        contentModule.map((content) => (
+          <Section contentModuleId={content.id} type={content.__typename} key={content.id} />
+        ))}
+    </Layout>
+  )
+}
 
 export default NotFoundPage
